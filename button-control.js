a5650b29dd7f09babcf1eab3562754d5d6736385
clara <clarisaharguindey@gmail.com>
2020-01-18 20:25:08 +0100

    Add several buttons

--- button-control.js
+++ button-control.js
@@ -1,10 +1,19 @@
-const button = require('rpi-gpio-buttons')([3]);
+const button = require('rpi-gpio-buttons')([3, 11, 29, 33]);
 const EventEmitter = require('events').EventEmitter;
 const buttonEventEmitter = new EventEmitter();
 
 button.on('clicked', function(pin) {
     console.log(pin, 'clicked');
-    buttonEventEmitter.emit('event', 'param1', 'param2');
+    switch(pin){
+        case 3:
+            buttonEventEmitter.emit('event', 'reacción 3 registrada', '😡');
+        case 11:
+            buttonEventEmitter.emit('event', 'reacción 11 registrada', '🧚‍♀️');
+        case 29:
+            buttonEventEmitter.emit('event', 'reacción 29 registrada', '🐷');
+        case 33:
+            buttonEventEmitter.emit('event', 'reacción 33 registrada', '🐤');
+    }
 })
 
 exports.buttonEventEmitter = buttonEventEmitter;
