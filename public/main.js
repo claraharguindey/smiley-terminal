add9a26ae337757354a8076bc953adf466a49172
claraharguindey <clarisaharguindey@gmail.com>
2020-02-29 20:47:35 +0100

    Print reactions correctly

--- public/main.js
+++ public/main.js
@@ -5,19 +5,46 @@ const socket = io.connect('http://localhost:4000');
 const output = document.getElementById('output');
 let answers;
 let packs;
+let packsCounter = 0;
+let reactionsCounter = 0;
+let currentReaction;
+let currentReactionId;
+let currentPack;
+let currentPackId;
 
-function getAnswersAndPacks () {
-    fetch('http://localhost:4000/answers').then((result) => result.json()).then((json) => answers = json);
-    fetch('http://localhost:4000/reactions').then((result) => result.json()).then((json) => packs = json);
+fetch('http://localhost:4000/answers').then((result) => result.json()).then((json) => answers = json);
+fetch('http://localhost:4000/reactions').then((result) => result.json()).then((json) => packs = json);
+
+function getCurrentReaction() {
+    currentPack = packs[packsCounter];
+    currentPackId = currentPack.id;
+    const reactionsFromPack = JSON.parse(currentPack.reactions_ids);
+    currentReactionId = reactionsFromPack[reactionsCounter];
+    fetch(`http://localhost:4000/reactions/${currentReactionId}`).then((result) => result.json()).then((json) => {
+        currentReaction=json[0];
+        printReaction();
+    });
+
+    if (reactionsCounter < 5) {
+        reactionsCounter++
+    } else {
+        output.innerHTML = '<p>' + '¡Valora tu experiencia!'+ '</p>';
+        reactionsCounter = 0;
+        if (packsCounter < packs.length) {
+            packsCounter++
+        } else {
+            packsCounter = 0;
+        }
+    } 
 }
 
+function printReaction() {
+    output.innerHTML = '<p>' + currentReaction.text+ '</p>';
+}
 
-
-// Listen for events
 socket.on('output', function(data) {
     if (data.event === 'buttonClicked') {
-        //output.innerHTML = '<p><strong>' + data.event + ': </strong>' + data.value + '</p>';
-        output.innerHTML = '<p><strong>' + 'answers' + ': </strong>' + packs + '</p>';
+        getCurrentReaction();
     }
 }
 );
